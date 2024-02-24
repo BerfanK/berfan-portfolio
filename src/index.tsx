94a6194ecb1c5b5909065a138e94d63c54bd17a5
Berfan Korkmaz <berfan.korkmaz@stud.edubs.ch>
2024-02-24 18:55:29 +0100

    Closed website

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout.tsx';
 import './lib/i18n';
 
 /* CSS */
 import './index.css';
 
 /* Pages */
-import Home from './pages/Home.tsx';
-import About from './pages/About.tsx';
-import Projects from './pages/Projects.tsx';
+import Maintenance from './pages/Maintenance.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Maintenance />} />
+            </Routes>
+            {/*
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                     <Route path="projects" element={<Projects />} />
                 </Routes>
             </Layout>
+            */}
         </BrowserRouter>
     </React.StrictMode>
 );
---
+++ src/pages/Maintenance.tsx
@@ -0,0 +1,7 @@
+export default function Maintenance() {
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <h1 className="font-semibold text-black text-2xl">In Construction...</h1>
+        </div>
+    );
+}
